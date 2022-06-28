9d49b4c18b38c66c90b5d467a04c3f63a445aee7
matyaspr <matyaspr@gmail.com>
2022-06-28 16:50:08 -0300

    ADD models for artist musical

---
+++ Model/Bands.js
@@ -0,0 +1,58 @@
+const Band = require("./Band");
+
+
+
+class Bands {
+
+    constructor() {
+            
+        this.bands = [
+            new Band("Metallica"),
+            new Band("Iron Maiden"),
+            new Band("AC/DC"),
+            new Band("Pink Floyd"),
+        ];
+    }
+
+
+    addBand(bandName) {
+        this.bands.push(new Band(bandName));
+        
+        return this.bands;
+    }
+
+    removeBand(bandId) {
+        this.bands = this.bands.filter(band => band.id !== bandId);
+        return this.bands;
+    }
+
+    getBands() {
+        return this.bands;
+    }
+
+    increaseVote(bandId) {
+        this.bands.forEach(band => {
+            if (band.id === bandId) {
+                band.votes++;
+            }
+        });
+        
+        return this.bands;
+    }
+
+
+    changeName(bandId, newName) {
+        this.bands.forEach(band => {
+            if (band.id === bandId) {
+                band.name = newName;
+            }
+        });
+        
+        return this.bands;
+    }
+}
+
+
+
+
+module.exports = Bands;
